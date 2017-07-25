67a6622876c0afc92cdbb3ea2514c34893d192d5
Wesley-M <wesleylino99@outlook.com>
2017-07-25 08:25:05 -0300

    login e outras modificações na tela inicial

--- js/form_js.js
+++ js/form_js.js
@@ -7,14 +7,14 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-  $('#nascimento, #data_p, #data_c').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-  });
+$('#dt-ida, #dt-volta, #nascimento, #data_p, #data_c').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 15, // Creates a dropdown of 15 years to control year,
+  today: 'Today',
+  clear: 'Clear',
+  close: 'Ok',
+  closeOnSelect: false // Close upon selecting a date,
+});
 
 
 $(document).ready(function(){
