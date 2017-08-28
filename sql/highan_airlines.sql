beb153f4a17f153b65aaff439e478e815be60c1f
Wesley-M <wesleylino99@outlook.com>
2017-08-28 14:15:05 -0300

    translation and incrementation

--- sql/highan_airlines.sql
+++ sql/highan_airlines.sql
@@ -1,61 +1,68 @@
-CREATE DATABASE highan_airlines;
-USE highan_airlines;
+create schema highan_airlines;
+use highan_airlines;
 
-
-CREATE TABLE usuario (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
-	way_to_image VARCHAR(255) NOT NULL,
-	cpf VARCHAR(14) NOT NULL,
-	senha (40) NOT NULL,
-	endereco VARCHAR(255) NOT NULL,
-	telefone VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	data_nascimento DATE NOT NULL,
-	rg VARCHAR(255) NOT NULL,
-	nome_completo VARCHAR(255)
+create table Airplanes(
+	Id INT PRIMARY KEY auto_increment NOT NULL,
+    Type VARCHAR(255) NOT NULL,
+    Description VARCHAR(255) NOT NULL,
+    Total_seats INT NOT NULL,
+    Provider VARCHAR(255) NOT NULL,
+    Status VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE funcao (
-	id_func INTEGER NOT NULL,
-	local_trabalho VARCHAR(255) NOT NULL,
-	id_usuario INTEGER NOT NULL,
-	nome VARCHAR(255),
-	salario REAL NOT NULL,
-	FOREIGN KEY(id_usuario) REFERENCES usuario(id)
+create table Users(
+	Id INT PRIMARY KEY auto_increment,
+    CPF VARCHAR(14) UNIQUE NOT NULL,
+    RG VARCHAR(255) UNIQUE NOT NULL,
+	Name VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Address VARCHAR(255) NOT NULL,
+    Phone VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Birthday DATE NOT NULL,
+    Id_function INT,
+
+	FOREIGN KEY(Id_function) REFERENCES Function(Id)
 );
 
-CREATE TABLE aeronave (
-	way_to_image VARCHAR(255) NOT NULL,
-	id INTEGER NOT NULL PRIMARY KEY,
-	tipo INTEGER NOT NULL,
-  descricao VARCHAR(255) NOT NULL,
-  total_assentos INTEGER NOT NULL
+create table Functions(
+	Id INT PRIMARY KEY auto_increment NOT NULL,
+	Name VARCHAR(255) NOT NULL,
+    Salary FLOAT(2) NOT NULL,
+    Worlplace VARCHAR(255) NOT NULL,
+    Manager INT NOT NULL,
+
+    FOREIGN KEY(Manager) REFERENCES Users(Id)
 );
 
+create table Flights(
+		Id INTEGER NOT NULL PRIMARY KEY,
+		Origin VARCHAR(255) NOT NULL,
+    Destiny VARCHAR(255) NOT NULL,
+    Departure_time TIME NOT NULL,
+    Arrival_time TIME NOT NULL,
+		Departure_date DATE NOT NULL,
+    Arrival_date DATE NOT NULL,
+    Id_airplane INTEGER NOT NULL,
+    Id_pilot INTEGER NOT NULL,
 
-CREATE TABLE voo (
-	id INTEGER NOT NULL PRIMARY KEY,
-	origem VARCHAR(255) NOT NULL,
-  destino VARCHAR(255) NOT NULL,
-  hora_partida TIME NOT NULL,
-  hora_chegada TIME NOT NULL,
-	data_saida DATE NOT NULL,
-  data_chegada DATE NOT NULL,
-  id_aeronave INTEGER NOT NULL,
-  id_piloto INTEGER NOT NULL,
-
-	FOREIGN KEY(id_piloto) REFERENCES usuario(id),
-	FOREIGN KEY(id_aeronave) REFERENCES aeronave(id)
+		FOREIGN KEY(Id_airplane) REFERENCES Airplanes(id),
+		FOREIGN KEY(Id_pilot) REFERENCES Users(id)
 );
 
-CREATE TABLE passagem (
-	id_voo INTEGER NOT NULL,
-	preco FLOAT(4) NOT NULL,
-	nome VARCHAR (255) NOT NULL,
-	data_nascimento DATE NOT NULL,
-	telefone VARCHAR(30),
-	celular VARCHAR(30),
-	id_pass INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+create table Flight_tickets(
+	Id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	Price FLOAT(4) NOT NULL,
 
-	FOREIGN KEY (id_voo) REFERENCES voo(id)
+	Client_name VARCHAR (255) NOT NULL,
+	Client_CPF VARCHAR (255) NOT NULL,
+	Client_birthday DATE NOT NULL,
+	Client_telephone VARCHAR(30),
+	Client_cellphone VARCHAR(30),
+
+	Id_flight INTEGER NOT NULL,
+	Id_client INTEGER NOT NULL,
+
+	FOREIGN KEY (Id_flight) REFERENCES Flights(Id),
+	FOREIGN KEY (Id_client) REFERENCES Users(Id)
 );
